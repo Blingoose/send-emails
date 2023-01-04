894a854b90962ec05f3eb80de00b9d029ad62716
Andre Katov <andy.katov@icloud.com>
2023-01-05 00:22:16 +0200

    added .env && configured nodemailer

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import { notFound } from "./middleware/not-found.js";
 import { errorHandlerMiddleware } from "./middleware/error-handler.js";
 import { BadRequest } from "./errors/BadRequest.js";
 import { sendEmail } from "./controller/sendEmails.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 8000;
--- controller/sendEmails.js
+++ controller/sendEmails.js
@@ -1,4 +1,24 @@
 import { asyncWrapper } from "../middleware/asyncWrapper.js";
+import nodemailer from "nodemailer";
+
 export const sendEmail = asyncWrapper(async (req, res, next) => {
-  res.send("Send email func");
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: "Blingoose <blingoose@gmail.com>",
+    to: "<staff@example.com>",
+    subject: "Hello World-LAST_ONE-3",
+    html: "<h2>Testing message, can you see me now?</h2>",
+  });
+
+  res.json(info);
 });
