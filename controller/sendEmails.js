8910bcf1b0f22b88e1a854110901c3b4c5db3b2a
Andre Katov <andy.katov@icloud.com>
2023-01-06 18:41:45 +0200

    adding sendgrid services

--- controller/sendEmails.js
+++ controller/sendEmails.js
@@ -1,24 +1,40 @@
+// import nodemailer from "nodemailer";
 import { asyncWrapper } from "../middleware/asyncWrapper.js";
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
 
+// export const sendEmail = asyncWrapper(async (req, res, next) => {
+//   let testAccount = await nodemailer.createTestAccount();
+
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     auth: {
+//       user: process.env.MAIL_USER,
+//       pass: process.env.PASSWORD,
+//     },
+//   });
+
+//   let info = await transporter.sendMail({
+//     from: "Blingoose <blingoose@gmail.com>",
+//     to: "<staff@example.com>",
+//     subject: "Hello World-LAST_ONE-3",
+//     html: "<h2>Testing message, can you see me now?</h2>",
+//   });
+
+//   res.json(info);
+// });
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export const sendEmail = asyncWrapper(async (req, res, next) => {
-  let testAccount = await nodemailer.createTestAccount();
+  const msg = {
+    to: "andy.katov@icloud.com",
+    from: "andy.katov@blingoose.net",
+    subject: "This is a test",
+    text: "Can you see this message?",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.PASSWORD,
-    },
-  });
-
-  let info = await transporter.sendMail({
-    from: "Blingoose <blingoose@gmail.com>",
-    to: "<staff@example.com>",
-    subject: "Hello World-LAST_ONE-3",
-    html: "<h2>Testing message, can you see me now?</h2>",
-  });
+  let info = await sgMail.send(msg);
 
   res.json(info);
 });
